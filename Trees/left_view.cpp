4bbc94162497ea70f2bb58ab4dca5aed991c230e
pankhuri-52 <pankhuritrikha@gmail.com>
2019-11-04 15:11:43 +0530

    Left view of a tree

--- Trees/left_view.cpp
+++ Trees/left_view.cpp
@@ -1,61 +1,63 @@
 #include <iostream>
-#include <string.h>
+#include <queue>
 using namespace std;
 class Node{
 	public:
-		string data;
+		int data;
 		Node* next[2];
-		Node(string data){
+		Node(int data){
 			this->data=data;
 			next[0]=NULL;
 			next[1]=NULL;
 		}
 };
-int height(Node* root){
-	if(root==NULL)
-		return 0;
-	int l=height(root->next[0]);
-	int r=height(root->next[1]);
-	if(l>r)
-		return (l+1);
-	else
-		return (r+1);
-}
-void print_nodes(Node* root,int level){
-	if(root==NULL)
-		return;
-	else if(level==1)
-		cout<<root->data<<" ";
-	else{
-		print_nodes(root->next[0],level-1);
-		print_nodes(root->next[1],level-1);
-	}
-}
-void print(Node* root){
-	int h=height(root);
-	for(int i=1;i<=h;i++){
-		print_nodes(root,i);
-	}
-}
-Node* construct_binary(string arr[],Node* root,int i,int n){
-	if(i<n){
-		Node* new_node=new Node(arr[i]);
-		root=new_node;
-		if(arr[2*i+1]!="_")
-			root->next[0]=construct_binary(arr,root->next[0],2*i+1,n);
-		if(arr[2*i+2]!="_")
-			root->next[1]=construct_binary(arr,root->next[1],2*i+2,n);
-	}
+Node* construct_binary(Node* root,int arr[],int start,int end){
+	if(start>end)
+	return NULL;
+	root=new Node(arr[start]);
+	if(2*start+1<end && arr[2*start+1]!=-1)
+		root->next[0]=construct_binary(root->next[0],arr,2*start+1,end);
+	if(2*start+2<end && arr[2*start+2]!=-1)
+		root->next[1]=construct_binary(root->next[1],arr,2*start+2,end);
 	return root;
 }
-int main(int argc,const char *argv[]){
-	int n=argc-1;
-	int k=0;
-	string arr[argc];
-        for(int i=1;i<argc;i++){
-		arr[k++]=argv[i];
+void print_level(Node* root){
+	if(root==NULL)
+		return;
+
+	queue<Node*>q;
+	Node* curr;
+	q.push(root);
+	q.push(NULL);
+	cout<<root->data<<" ";
+	while(q.size()>1){
+		curr=q.front();
+		if(curr!=NULL)
+                   q.pop();
+		if(curr==NULL){
+			q.pop();
+			cout<<q.front()->data<<" ";
+			q.push(NULL);
+			//cout<<"\n";
+		} else {
+			if(curr->next[0])
+				q.push(curr->next[0]);
+			if(curr->next[1])
+				q.push(curr->next[1]);
+			/*if(curr->next[0]!=NULL)
+				cout<<curr->next[0]->data<<" ";
+			else
+				cout<<curr->next[1]->data<<" ";*/
+		}
 	}
-	Node* root=construct_binary(arr,root,0,n);
-	print(root);
+}
+int main(int argc,const char *argv[]){
+	int arr[argc];
+	int k=0;
+	for(int i=1;i<argc;i++){
+		arr[k++]=atoi(argv[i]);
+	}
+	Node* root=construct_binary(root,arr,0,argc-1);
+	print_level(root);
 	return 0;
 }
