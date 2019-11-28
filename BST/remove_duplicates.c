7f7ae1cb26fd6960e47b3c9f2bc02adefb28cffa
pankhuri-52 <pankhuritrikha@gmail.com>
2019-11-28 15:42:25 +0530

    Find maximum count of duplicate nodes in a Binary Search Tree(End Term)

--- BST/remove_duplicates.c
+++ BST/remove_duplicates.c
@@ -5,37 +5,54 @@ struct  Node{
 	int data;
 	struct Node* next[2];
 };
+struct Node* new_Node(int value){
+	struct Node* node=(struct Node*)malloc(sizeof(struct Node));
+	node->data=value;
+	node->next[0]=NULL;
+	node->next[1]=NULL;
+}
 struct Node* construct_tree(struct Node* root,int arr[],int start,int end){
-	printf("Entered");
 	if(start>end)
 		return NULL;
-	root=(struct Node*)malloc(sizeof(struct Node));
-	root->data=arr[start];
-	root->next[0]=NULL;
-	root->next[1]=NULL;
+	root=new_Node(arr[start]);
 	if(2*start+1<end && arr[2*start+1]!=-1)
 		root->next[0]=construct_tree(root,arr,2*start+1,end);
 	if(2*start+2<end && arr[2*start+2]!=-1)
 		root->next[1]=construct_tree(root,arr,2*start+2,end);
 	return root;
 }
-/*void inorder(struct Node* root){
+struct Node* previous=NULL;
+int curr=1;
+int max=0;
+int node;
+int inorder(struct Node* root){
 	if(root==NULL)
-		return;
+		return -1;
 	inorder(root->next[0]);
-	printf("%d ",root->data);
+	if(previous!=NULL){
+		if(root->data==previous->data)
+			curr++;
+		else
+			curr=1;
+	}
+	if(curr>max){
+		max=curr;
+               node=root->data;
+	}
+	previous=root;
 	inorder(root->next[1]);
-}*/
-int main(int main,int argc,const char *argv[]){
+	return node;
+}
+int main(int argc,const char *argv[]){
 	int arr[argc];
 	int k=0;
 	for(int i=1;i<argc;i++){
 		arr[k++]=atoi(argv[i]);
-	}
-	
-	struct Node* root=NULL;
+	}	
+	struct Node* root=(struct Node*)malloc(sizeof(struct Node));
+	root=NULL;
 	root=construct_tree(root,arr,0,argc-1);
-	printf("Hello");
-	//inorder(root);
+	int max=inorder(root);
+	printf("%d",max);
 	return 0;
 }
