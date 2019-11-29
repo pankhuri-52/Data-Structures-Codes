0dafaf1dbe79fc9bf242d6747376d50c421b2bcd
pankhuri-52 <pankhuritrikha@gmail.com>
2019-11-29 16:08:28 +0530

    Deletion from a heap

--- Heap/Insertion_in_heap.c
+++ Heap/Insertion_in_heap.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 int heap[100];
 int heapSize=-1;
+void swap(int a,int b){
+	int temp=a;
+	a=b;
+	b=temp;
+}
 void insert(int element){
      heapSize++;
      heap[heapSize]=element;
@@ -11,6 +16,25 @@ void insert(int element){
 	     now=(now-1)/2;
      }
      heap[now]=element;
+}
+void heapify(int heap[],int n,int i){
+     int smallest=i;
+     int l=2*i+1;
+     int r=2*i+2;
+     if(l<n && heap[l]<heap[smallest])
+	     smallest=l;
+     if(r<n && heap[r]<heap[smallest])
+	     smallest=r;
+     if(smallest!=i){
+	     swap(heap[i],heap[smallest]);
+	     heapify(heap,n,smallest);
+     }
+}
+void delete_element(int heap[],int n){
+	int last_element=heap[n-1];
+	heap[0]=last_element;
+	n=n-1;
+	heapify(heap,n,0);
 }
 int main(int argc,const char *argv[]){
 	int n=atoi(argv[1]);
@@ -22,5 +46,10 @@ int main(int argc,const char *argv[]){
 		printf("%d ",heap[i]);
 	}
 	printf("\n");
+	delete_element(heap,n);
+	printf("After deletion\n");
+	for(int i=0;i<n-1;i++){
+		printf("%d ",heap[i]);
+	}
 	return 0;
 }
