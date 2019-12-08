bae0fcd27580f6e81e55f459d5606bbe9a046bd6
pankhuri-52 <pankhuritrikha@gmail.com>
2019-12-08 15:25:31 +0530

    Arrange positive and negative at even and odd positions(End Term)

--- Arrays/positive_negative.c
+++ Arrays/positive_negative.c
@@ -1,40 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-void swap(int a,int b){
-    int temp=a;
-    a=b;
-    b=temp;
+void swap(int *a,int *b){
+	int temp=*a;
+	*a=*b;
+	*b=temp;
 }
-void rearrange(int a[], int size) 
-{ 
-    int positive = 0, negative = 1; 
-  
-    while (1) { 
-        while (positive < size && a[positive] >= 0) 
-            positive += 2; 
-  
-        while (negative < size && a[negative] <= 0) 
-            negative += 2; 
-  
-        if (positive < size && negative < size) 
-            swap(a[positive], a[negative]); 
-  
-        else
-            break; 
-    } 
-} 
-  
-int main(int argc,const char *argv[]) 
-{ 
-    int arr[argc];
-    int k=0;
-    for(int i=1;i<argc;i++){
-        arr[k++]=atoi(argv[i]);
-    }
-    rearrange(arr, k);  
-    for (int i = 0; i < k-1; i++) 
-        printf("%d ",arr[i]);
-    printf("%d",arr[k-1]);
-  
-    return 0; 
-} 
+void arrange(int arr[],int k){
+	int positive=0;
+	int negative=1;
+	while(true){
+		while(positive<k && arr[positive]>=0){
+			positive=positive+2;
+		}
+		while(negative<k && arr[negative]<=0){
+			negative=negative+2;
+		}
+		if(positive<k && negative<k)
+			swap(&arr[positive],&arr[negative]);
+		else
+			break;
+	}
+}
+int main(int argc,const char *argv[]){
+	int arr[argc];
+	int k=0;
+	for(int i=1;i<argc;i++){
+		arr[k++]=atoi(argv[i]);
+	}
+	arrange(arr,k);
+	for(int i=0;i<k-1;i++){
+		printf("%d ",arr[i]);
+	}
+	printf("%d",arr[k-1]);
+	return 0;
+}
+
