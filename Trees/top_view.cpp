fca37546464331b64f40e1bef7616eba657f1e18
pankhuri-52 <pankhuritrikha@gmail.com>
2020-02-11 12:00:32 +0530

    Top view of a binary tree

---
+++ Trees/top_view.cpp
@@ -0,0 +1,125 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void topView(struct Node *root);
+
+
+int main()
+{
+    int t;cin>>t;
+    while(t--)
+    {
+        int n,i,k;
+        cin>>n;
+        i=n;
+
+        Node* root=NULL;
+        queue <Node*> q;
+        while(i>0)
+        {
+            int a,b;
+            char c;
+            cin>> a >> b >> c;
+            if(!root){
+                root = new Node(a);
+                q.push(root);
+            }
+            Node* pick = q.front();
+            q.pop();
+
+            if(c == 'L'){
+                pick->left = new Node(b);
+                q.push( pick->left );
+            }
+            cin>> a >> b >> c;
+            if(c == 'R'){
+                pick->right = new Node(b);
+                q.push( pick->right );
+            }
+            i-=2;
+        }
+//        inorder(root);
+//        cout<<endl;
+        topView(root);
+        cout << endl;
+    }
+    return 0;
+}
+
+
+
+
+// } Driver Code Ends
+//Structure of binary tree
+/*struct Node
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+// function should print the topView of the binary tree
+void topView(struct Node *root)
+{
+    if(root==NULL)
+     return;
+    queue<pair<Node*,int>>q;
+    
+    pair<Node*,int>p;
+    p.first=root;
+    p.second=0;
+    map<int,int>m;
+    
+    q.push(p);
+    while(q.empty()==false){
+        pair<Node*,int> l=q.front();
+        q.pop();
+        Node* temp=l.first;
+        int hd=l.second;
+        
+        if(m.find(hd)==m.end())
+         m[hd]=temp->data;
+         
+        if(temp->left){
+            pair<Node*,int> k;
+            k.first=temp->left;
+            k.second=hd-1;
+            q.push(k);
+            
+        }
+        
+        if(temp->right){
+            pair<Node*,int> k;
+            k.first=temp->right;
+            k.second=hd+1;
+            q.push(k);
+        }
+    }
+    
+    map<int,int>::iterator itr;
+    for(itr=m.begin();itr!=m.end();itr++){
+        cout<<itr->second<<" ";
+    }
+}
+
+
+
