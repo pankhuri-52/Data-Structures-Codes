ff1490165a8f3f1593a1a99ceb0b8726d44db9b7
Pankhuri Trikha <49311757+pankhuri-52@users.noreply.github.com>
2020-03-15 20:47:48 +0530

    Parent of a given node
    Approach:
    Initialize the parent as -1 and keep the track of the parent, if the value of the node's data is equal to the given value then print the parent, else call recursively to the left and right subtree.

--- Trees/parent_of_node.cpp
+++ Trees/parent_of_node.cpp
@@ -1,51 +1,37 @@
-#include <iostream>
-using namespace std;
-class Node{
-    public:
-    int data;
-    Node* next[2];
-    Node(int data){
-        this->data=data;
-        next[0]=NULL;
-        next[1]=NULL;
-    }
-};
-Node* construct_tree(Node* root,int arr[],int start,int end){
-    if(start>end)
-    return NULL;
-    root=new Node(arr[start]);
-    if(2*start+1<end && arr[2*start+1]!=0)
-    root->next[0]=construct_tree(root,arr,2*start+1,end);
-    if(2*start+2<end && arr[2*start+2]!=0)
-    root->next[1]=construct_tree(root,arr,2*start+2,end);
-    return root;
-}
-void print_parent(int key,int arr[],int k,Node* root){
-    int parent;
-    if(root==NULL)
-    return;
-    if(root->data==key){
-        cout<<"-1";
-        return;
-    }
-    else{
-    for(int i=1;i<k;i++){
-        if(arr[i]==key){
-        parent=(i-1)/2;
-        break;
-        }
-    }
-    cout<<arr[parent];
-    }
-}
-int main(int argc,const char *argv[]){
-    int key=atoi(argv[1]);
-    int arr[argc];
-    int k=0;
-    for(int i=2;i<argc;i++){
-        arr[k++]=atoi(argv[i]);
-    }
-    Node* root=construct_tree(root,arr,0,k);
-    print_parent(key,arr,k,root);
-    return 0;
-}
+#include <iostream> 
+using namespace std; 
+struct Node { 
+	int data; 
+	struct Node *left, *right; 
+	Node(int data) 
+	{ 
+		this->data = data; 
+		left = right = NULL; 
+	} 
+}; 
+void findParent(struct Node* node, 
+				int val, int parent) 
+{ 
+	if (node == NULL) 
+		return; 
+	if (node->data == val) { 
+		cout << parent; 
+	} 
+	else { 
+		findParent(node->left, val, node->data); 
+		findParent(node->right, val, node->data); 
+	} 
+} 
+int main() 
+{ 
+	struct Node* root = new Node(1); 
+	root->left = new Node(2); 
+	root->right = new Node(3); 
+	root->left->left = new Node(4); 
+	root->left->right = new Node(5); 
+	int node = 3; 
+
+	findParent(root, node, -1); 
+
+	return 0; 
+} 
